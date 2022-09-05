993db8c24bea2492c186b74536b996c3ba695b30
NavarreteAndres <112371339+NavarreteAndres@users.noreply.github.com>
2022-09-04 19:19:16 -0700

    Add files via upload

---
+++ Meta 1.3/Actividad 1.3.1.py
@@ -0,0 +1,5 @@
+num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+oxxos=["paseos del florido","nicolas bravo","laurel","cañadas","beta","abedules","hdli","vid","girasol","polen","sauce","los encinos","insurgentes","tolteca","guaycura","franciscoI.madero","san pedro","cochimies","finisterra","murua","emiliano zapata","loma dorada","josefa ortiz","casa blanca","cucapah"]
+cp=[22244,22170,22245,22237,22238,22245,22248,24409,22245,22234,22254,22245,22235,22236,22303,22236,22237,22234,22244,22243,22240,22301,22250,22263,22220]
+relacion=list(zip(num,oxxos,cp))
+print(relacion)
