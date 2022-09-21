ae5863e759991e8193cfdc3a2614c5fe8dffb416
NavarreteAndres <112371339+NavarreteAndres@users.noreply.github.com>
2022-09-20 21:24:59 -0700

    Add files via upload

---
+++ Meta 2.1/Actividad 2.1.1.py
@@ -0,0 +1,14 @@
+a=10
+b=5
+fun1= (((3+5*8)<3) and ((-6/3*4)+2<2)) or (a>b)
+print(fun1)
+fun2= (((2+5*7)>3) or ((-5/7*5)+2>2)) and (a<b)
+print(fun2)
+fun3= (((4+5*6)<=3) or not ((-4/6*6)+2<=2)) or (a>=b)
+print(fun3)
+fun4= (((6-5*5)>=3) and ((-3/5*7)+2>=2)) or not (a<=b)
+print(fun4)
+fun5= (((8-5*4)<3) and ((-2/4*8)+2<2)) and (a==b)
+print(fun5)
+fun6= (((3-5*3)>3) or ((-1/3*9)+2>2)) or (a!=b)
+print(fun6)
