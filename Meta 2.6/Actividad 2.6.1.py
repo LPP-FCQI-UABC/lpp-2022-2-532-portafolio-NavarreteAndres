333cd8d5a393f3bacad092d3354918f29bbc2100
NavarreteAndres <112371339+NavarreteAndres@users.noreply.github.com>
2022-11-06 17:55:48 -0800

    Add files via upload

---
+++ Meta 2.6/Actividad 2.6.1.py
@@ -0,0 +1,12 @@
+personajes = [] 
+
+p = {"Nombre":"Aragorn","Clase":"Guerrero","Raza":"Dúnadan del norte"}
+personajes.append(p)  
+
+p = {"Nombre":"Gandalf","Clase":"Mago","Raza":"Istar"}
+personajes.append(p)  
+
+p = {"Nombre":"Legolas","Clase":"Arquero","Raza":"Elfo Sindar"}
+personajes.append(p)  
+
+print(personajes)
